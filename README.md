3fe3404eb04b7b675fae9391728bed5503959e1e
HaoRyou <158393369+HaoRyou@users.noreply.github.com>
2025-07-07 18:32:23 -0400

    modified: style and index is mainly modified After researching, found another way to build calculator with less function Try to build calculator using the given guide.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Calculator
+
+The user should be able to click on button and it will display to the user. When the user enter two number and click on a function, it will return a output.
--- main.js
+++ main.js
@@ -13,7 +13,7 @@ function divide(a,b){
     return a/b;
 }
 
-function operate(num1,choice, num2){
+function operate(num1, choice, num2){
     if(choice == "*"){
         return multiply(num1,num2);
     }
@@ -27,4 +27,29 @@ function operate(num1,choice, num2){
         return subtract(num1-num2);
     }
 
+}
+
+function nextnum(a){
+    if(a=='*' || a=='/' || a=='+' || a == '-'){
+        
+    }
+}
+
+const outputs = document.getElementById("Output");
+
+function input(a){
+    outputs.value += a;
+}
+
+function clear_screen(){
+    outputs.value = "";
+}
+
+function Finish(){
+    try{
+        outputs.value = eval(outputs.value);
+    }
+    catch(error){
+        outputs.value = "Error";
+    }
 }
